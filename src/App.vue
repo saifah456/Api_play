361f621d58ad8ef38df4e01a3f0afb4d9b94cfb6
saifah456 <saifah456@gmail.com>
2022-08-27 01:45:34 +0530

    Api called

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <greetNow  name="Saif" lname="Ahmad"/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import greetNow from './components/greet.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    greetNow
+
   }
 }
 </script>
---
+++ src/components/greet.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+    
+    Hello My name is :{{name}} {{lname}}
+
+    <button @click="callApi">API</button>
+    Api Data: 
+    {{apiData}} 
+    
+
+</div>
+
+
+</template>
+<script>
+
+ import axios from 'axios'
+
+ 
+export default {
+  name: 'greetNow',
+
+  props:{
+    name:String,
+    lname:String
+  },
+   
+   data(){
+        return{
+            apiData : "",
+        }
+   },
+  methods: {
+     callApi(){
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        .then(  // on resolve
+            (api_data)=>{
+                console.log("API Data" ,api_data.data)
+                this.apiData=api_data.data
+
+            }
+        )
+        .catch(  // on error/ reject
+            function rem(error){
+                console.log("API Error",error)
+            }
+        )
+     }
+  },
+  
+}
+</script>
+
